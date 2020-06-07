a2dd40060adb1539d257cd65572a60f5f09c0f07
vallacy <valryan15@gmail.com>
2020-06-07 14:29:12 -0400

    initial commit

---
+++ R Code DnD Dice Rolls.R
@@ -0,0 +1,41 @@
+##########################################
+########### Rolling Dice DnD #############
+#########################################
+
+#roll a d4 die once
+d4 <- sample(1:4, 1, replace=T)
+d4
+#hist(d4)
+
+#roll a d6 die once
+d6 <- sample(1:6, 1, replace=T)
+d6
+#hist(d6)
+
+#roll a d8 die once
+d8 <- sample(1:8, 1, replace=T)
+d8
+#hist(d8)
+
+#roll a d10 die once
+d10 <- sample(1:10, 1, replace=T)
+d10
+#hist(d10)
+
+#roll a d12 die once
+d12 <- sample(1:12, 1, replace=T)
+d12
+#hist(d12)
+
+#roll a d20 die once
+d20 <- sample(1:20, 1, replace=T)
+d20
+#hist(d20)
+
+####################quick rolls
+sample(1:4, 1, replace=T)
+sample(1:6, 1, replace=T)
+sample(1:8, 1, replace=T)
+sample(1:10, 1, replace=T)
+sample(1:12, 1, replace=T)
+sample(1:20, 1, replace=T)
